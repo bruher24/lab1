f480623dbcb3eeaf45094140b6488ca1e8dfe087
bruher24 <glushkovd2424@gmail.com>
2025-09-19 14:38:56 +0400

    IT WORKS! it remains only to add "show full db" feature

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import uuid
 from tkinter import *
 import sqlite3
 
@@ -9,19 +10,27 @@ class Game:
         self.init_db()
 
         # Загружаем существующие варианты
-        self.variants = self.load_creatures_from_db()
+        self.full_variants = self.load_creatures_from_db()
+
+        self.variants = self.build_tree(self.full_variants)
+
+        print(self.variants)
 
         # Инициализируем переменные
         self.current_answer = None
         self.waiting_for_answer = False
-        self.current_variant = -1
         self.waiting_for_name = False
+        self.current_obj = None
+        self.current_variant = None
         self.text_buffer = None
-        self.new_name = None
-        self.new_question = None
+        self.new_parent_answer = None
         self.new_answer = None
+        self.new_question = None
+        self.new_name = None
+        self.last_answer = None
         self.adding_new = False
         self.log = []
+
         # Создаем основное окно
         self.root = tk.Tk()
         self.root.title('Quiz')
@@ -44,6 +53,7 @@ class Game:
 
         # Кнопка отображения схемы БД
         self.btnBD = tk.Button(self.root, text='Показать БД', fg='black', command=self.show_bd)
+        self.btnBD.place(x=390, y=240, anchor='center')
 
         # Создаем кнопки Да/Нет
         self.btn_yes = tk.Button(self.root, text='Да', fg='green', command=self.yes_clicked)
@@ -54,6 +64,32 @@ class Game:
 
         # Создаем кнопку ввода
         self.btn_confirm = tk.Button(self.root, text='Подтвердить', fg='green', command=self.confirm_clicked)
+
+    # Формируем дерево из данных БД
+    def build_tree(self, variants):
+        nodes = {}
+        root = None
+
+        # Создаем узлы
+        for item in variants:
+            node_id = item['id']
+            nodes[node_id] = {
+                'id': node_id,
+                'is_question': item['is_question'],
+                'name': item['name'],
+                'yes': {},
+                'no': {}
+            }
+            if item['parent_id'] is None:
+                root = node_id
+        # Создаем связи
+        for item in variants:
+            if item['parent_id'] is not None:
+                parent = nodes[item['parent_id']]
+                key = 'yes' if item['parent_answer'] else 'no'
+                parent[key][str(item['id'])] = nodes[item['id']]
+
+        return {str(root): nodes[root]} if root is not None else {}
 
     # Запускаем игру
     def play(self):
@@ -69,13 +105,17 @@ class Game:
     def reset(self):
         self.current_answer = None
         self.waiting_for_answer = False
-        self.current_variant = -1
         self.waiting_for_name = False
+        self.current_obj = None
+        self.current_variant = None
         self.text_buffer = None
-        self.new_name = None
-        self.new_question = None
+        self.new_parent_answer = None
         self.new_answer = None
+        self.new_question = None
+        self.new_name = None
+        self.last_answer = None
         self.adding_new = False
+        self.log = []
         self.lbl.configure(text='Нажмите "Начать игру"')
         self.lbl.place(x=225, y=100, anchor='center')
         self.btn_start_game.place(x=225, y=150, anchor='center')
@@ -84,25 +124,37 @@ class Game:
         self.btn_confirm.place_forget()
         self.text_input.place_forget()
         self.btnLog.place_forget()
+        self.variants = self.build_tree(self.full_variants)
 
     # Отображение лога
     def show_log(self):
         output = ''
         self.log.pop(0)
+        counter = 0
         for word in self.log:
+            counter += 1
+
             if word == '0':
                 word = 'Нет'
             elif word == '1':
                 word = 'Да'
+
             output += word + '->'
 
-        output = output[:-2]
+            if counter == 5:
+                output += '\n'
+                counter = 0
+
+        if output[-2:] != '\n':
+            output = output[:-2]
+
         self.btnLog.place_forget()
         self.lbl.configure(text=output)
 
     # Отображение схемы БД
     def show_bd(self):
-        print(1)
+        output = self.build_tree(self.full_variants)
+        print(output)
 
     # Нажатие "Да"
     def yes_clicked(self):
@@ -135,7 +187,8 @@ class Game:
     def new_creature(self):
         if self.new_name is None:
             self.new_name = self.text_buffer.capitalize()
-            self.lbl.configure(text='Что отличает ' + self.new_name + '?')
+            self.lbl.configure(
+                text='Что отличает ' + self.new_name + ' от ' + self.current_obj['name'] + '?')
             self.text_input.delete(0, END)
             self.waiting_for_answer = True
         elif self.new_question is None:
@@ -149,8 +202,8 @@ class Game:
             self.btn_no.place(x=275, y=150, anchor='center')
             self.text_input.delete(0, END)
             self.waiting_for_answer = True
-        elif self.new_answer is None:
-            self.new_answer = self.current_answer
+        elif self.new_parent_answer is None:
+            self.new_parent_answer = self.current_answer
             self.save_new_creature()
             self.lbl.configure(text='Спасибо! Я узнал новое животное!')
             self.btn_no.place_forget()
@@ -158,25 +211,42 @@ class Game:
 
     # Сохраняем новое существо в БД
     def save_new_creature(self):
+        print(self.current_obj['id'])
+
         conn = self.get_db_connection()
         cursor = conn.cursor()
+        new_parent_id = str(uuid.uuid4())
 
-        cursor.execute("INSERT INTO variants (name, question, answer) VALUES (?, ?, ?)",
-                       (self.new_name, self.new_question, self.new_answer))
+        cursor.execute("""
+            INSERT OR IGNORE INTO variants (id, name, parent_answer, parent_id, is_question)
+            VALUES (?, ?, ?, ?, ?)
+        """,
+                       (new_parent_id, self.new_question, self.last_answer, str(self.current_obj['id']), 1))
+
+        cursor.execute("""
+                    INSERT OR IGNORE INTO variants (id, name, parent_answer, parent_id, is_question)
+                    VALUES (?, ?, ?, ?, ?)
+                """,
+                       (str(uuid.uuid4()), self.new_name, self.new_parent_answer, new_parent_id, 0))
+
         conn.commit()
         conn.close()
+
         self.variants = self.load_creatures_from_db()
 
     # Задаем вопрос, указанный для существа
     def ask_question(self):
-        question = self.variants[self.current_variant]['question']
+        self.current_obj = self.variants[str(self.current_variant)]
+        question = self.variants[str(self.current_variant)]['name']
         self.lbl.configure(text=question)
         self.waiting_for_answer = True
+        self.waiting_for_name = False
         self.log.append(question)
 
     # Предполагаем имя существа
     def ask_name(self):
-        question = 'Это ' + self.variants[self.current_variant]['name'] + '?'
+        self.current_obj = self.variants[str(self.current_variant)]
+        question = 'Это ' + self.variants[str(self.current_variant)]['name'] + '?'
         self.lbl.configure(text=question)
         self.waiting_for_answer = True
         self.waiting_for_name = True
@@ -200,15 +270,16 @@ class Game:
         self.new_name = None
         self.new_question = None
         self.new_answer = None
+        self.last_answer = self.current_answer
 
     # Побеждаем
     def win(self):
-        answer = 'Это точно ' + self.variants[self.current_variant]['name'] + '!'
+        answer = 'Это точно ' + self.variants[str(self.current_variant)]['name'] + '!'
         self.lbl.configure(text=answer)
         self.log.append(answer)
         self.btn_yes.place_forget()
         self.btn_no.place_forget()
-        self.btnLog.place(x=225, y=100, anchor='center')
+        self.btnLog.place(x=225, y=150, anchor='center')
 
     # Обрабатываем ответ
     def process_answer(self):
@@ -218,9 +289,9 @@ class Game:
         self.waiting_for_answer = False
 
         # Начало игры - первая попытка угадать
-        if self.current_variant == -1:
+        if self.current_variant is None:
             if self.current_answer == '1':
-                self.current_variant += 1
+                self.current_variant = 1
                 self.ask_name()
             else:
                 self.cant_start()
@@ -228,24 +299,25 @@ class Game:
 
         # Процесс игры - задаем вопросы
         if not self.waiting_for_name:
-            if str(self.current_answer) == str(self.variants[self.current_variant]['answer']):
+            if str(self.current_answer) == '1':
+                self.variants = self.variants[str(self.current_variant)]['yes']
+            else:
+                self.variants = self.variants[str(self.current_variant)]['no']
+            if len(self.variants) > 0:
+                self.current_variant = next(iter(self.variants))
                 self.ask_name()
             else:
-                self.current_variant += 1
-                if self.current_variant < len(self.variants):
-                    self.ask_question()
-                else:
-                    self.give_up()
+                self.give_up()
 
         # Попытка угадать название
         else:
             if self.current_answer == '1':
                 self.win()
             else:
-                self.current_variant += 1
-                if self.current_variant < len(self.variants):
+                self.variants = self.variants[str(self.current_variant)]['no']
+                if len(self.variants) > 0:
+                    self.current_variant = next(iter(self.variants))
                     self.ask_question()
-                    self.waiting_for_name = False
                 else:
                     self.give_up()
 
@@ -262,16 +334,17 @@ class Game:
 
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS variants (
-                id INTEGER PRIMARY KEY,
-                name VARCHAR NOT NULL UNIQUE,
-                question VARCHAR NOT NULL,
-                answer TINYINT NOT NULL
+                id VARCHAR PRIMARY KEY,
+                parent_id VARCHAR,
+                parent_answer TINYINT,
+                is_question TINYINT,
+                name VARCHAR NOT NULL UNIQUE
             )
         """)
 
         cursor.execute("""
-            INSERT OR IGNORE INTO variants (name, question, answer)
-            VALUES ('Кот', 'Мяукает?', 1)
+            INSERT OR IGNORE INTO variants (id, parent_id, parent_answer, is_question, name)
+            VALUES (1, NULL, NULL, 0, 'Кот')
         """)
 
         conn.commit()
